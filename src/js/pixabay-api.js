7e61e7e31890434cfa6d7829271a72cf77c0a9f9
HK-b-code <gkros13@gmail.com>
2025-02-18 10:41:07 +0100

    Update

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const API_KEY = '48892292-adf57a8bf9903d599c085e6e7';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export async function fetchImages(query) {
+  try {
+    const response = await axios.get(BASE_URL, {
+      params: {
+        key: API_KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+      },
+    });
+    return response.data.hits;
+  } catch (error) {
+    console.error('Error fetching images:', error);
+    return [];
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,69 @@
+import { fetchImages } from './js/pixabay-api';
+import { renderImages } from '/js/render-functions';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import messageSvg from './img/error-svg-1.svg';
+
+const searchForm = document.querySelector('#search-form');
+const loader = document.querySelector('.loader');
+const loadingMessage = document.querySelector('#loading-message');
+const searchButton = searchForm.querySelector('button');
+
+const iziToastStyle = {
+  messageColor: '#fafafb',
+  backgroundColor: '#ef4040',
+  messageSize: '16px',
+  timeout: 4000,
+  iconUrl: messageSvg,
+  position: 'topRight',
+  displayMode: 'replace',
+  transitionIn: 'flipInX',
+};
+
+function showLoader() {
+  loader.style.display = 'block';
+}
+
+function hideLoader() {
+  loader.style.display = 'none';
+}
+
+searchForm.addEventListener('submit', async e => {
+  e.preventDefault();
+  const searchQuery = e.target.elements.searchQuery.value.trim();
+
+  loadingMessage.style.display = 'none';
+
+  if (!searchQuery) {
+    iziToast.error({
+      ...iziToastStyle,
+      message: 'Please enter a search query!',
+    });
+    return;
+  }
+
+  showLoader();
+  loadingMessage.style.display = 'block';
+  searchButton.disabled = true;
+
+  try {
+    const images = await fetchImages(searchQuery);
+    if (images.length === 0) {
+      iziToast.error({
+        ...iziToastStyle,
+        message: 'Sorry, no images found. Try again.',
+      });
+    } else {
+      renderImages(images);
+    }
+  } catch (error) {
+    iziToast.error({
+      ...iziToastStyle,
+      message: 'Something went wrong, please try again later.',
+    });
+  } finally {
+    hideLoader();
+    loadingMessage.style.display = 'none';
+    searchButton.disabled = false;
+  }
+});
